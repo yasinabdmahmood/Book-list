879647b03711bde5f7ad78ac7e14ab47b7e96fde
yasinabdmahmood <abd181103@gmail.com>
2022-08-11 21:39:14 +0300

    Fix code inside removeBookFromLocalStorage method

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ class BookList {
   static removeBookFromLocalStorage(e) {
     const title = e.target.previousElementSibling.previousElementSibling.innerHTML;
     const arr = JSON.parse(localStorage.getItem('book list'));
-    const index = arr.findIndex((object) => object.title === title);
+    const index = arr.findIndex((object) => `"${object.title}" by` === title);
     arr.splice(index, 1);
     localStorage.setItem('book list', JSON.stringify(arr));
   }
